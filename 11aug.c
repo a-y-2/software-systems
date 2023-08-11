2138297c2083a853019f4286280da0f2e8faedb5
Ayushi Prasad <60753222+a-y-2@users.noreply.github.com>
2023-08-11 11:07:53 +0530

    Update 11aug.c

--- 11aug.c
+++ 11aug.c
@@ -4,13 +4,14 @@
 #include<sys/stat.h>
 #include<fcntl.h>
 int main(){
+	//lab2.txt should be existing and tempsoftlink should not exist
 	int v = symlink("lab2.txt","tempsoftlink");
 	if(v<0)
 		{perror("failed");return 1;}
 	int f=link("lab2.txt","temphardlink");
 	if(f<0)
 		{perror("failed");return 1;}
-	
+	//lab2.txt should not exist
 	int m=mknod("lab2.txt",S_IFIFO,0);
 	if(m<0)
 		{perror("failed");return 1;}
