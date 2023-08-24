7c858d9e204e2e767cb112ac24d5789ce5797732
Ayushi Prasad <60753222+a-y-2@users.noreply.github.com>
2023-08-25 00:23:50 +0530

    Add files via upload

---
+++ 12.c
@@ -0,0 +1,22 @@
+
+#include<sys/types.h>
+#include<fcntl.h>
+#include<sys/stat.h>
+#include<stdio.h>
+#include<errno.h>
+#include<time.h>
+#include<unistd.h>
+
+int main(int argc,char* argv[]){
+	
+	int fd = open(argv[1],O_RDWR);
+	int f = fcntl(fd,F_GETFL);
+	if(f&O_RDWR)
+		printf("read-write mode");
+	else
+		printf("not in read-write mode");
+
+	close(fd);
+	
+return 0;
+}
