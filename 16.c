0b61270758331ab2056f774be9d2bb7eaaaba123
Ayushi Prasad <60753222+a-y-2@users.noreply.github.com>
2023-08-29 08:39:15 +0530

    Add files via upload

---
+++ 16.c
@@ -0,0 +1,30 @@
+#include<sys/types.h>
+#include<fcntl.h>
+#include<sys/stat.h>
+#include<stdio.h>
+#include<errno.h>
+#include<time.h>
+#include<unistd.h>
+
+int main(int argc,char* argv[]){
+	int fd;
+	struct flock lock;
+	
+	fd = open(argv[1],O_RDWR);
+	lock.l_type = F_WRLCK;
+	lock.l_whence = SEEK_SET;
+	lock.l_start = 0;
+	lock.l_len = 0;
+	lock.l_pid = getpid();
+	printf("before entering into critical section\n");
+	fcntl(fd,F_SETLKW,&lock);
+	printf("inside critical section\n");
+	printf("press enter to unlock\n");
+	getchar();
+	printf("unlocked\n");
+	lock.l_type = F_UNLCK;
+	fcntl(fd,F_SETLK,&lock);
+	printf("end");
+		
+return 0;
+}
