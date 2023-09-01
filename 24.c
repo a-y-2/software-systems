bbc58e32cb7e24e9ec06484d34494aa99bd1fa4e
Ayushi Prasad <60753222+a-y-2@users.noreply.github.com>
2023-09-01 11:17:08 +0530

    Add files via upload

---
+++ 24.c
@@ -0,0 +1,23 @@
+#include<sys/types.h>
+#include<stdlib.h>
+#include<sys/stat.h>
+#include<stdio.h>
+#include<errno.h>
+#include<time.h>
+#include<unistd.h>
+
+int main(){
+
+	if(fork()==0){
+		printf("\n child process id : %d",getpid());
+		sleep(1);
+		printf("\n child process id after becoming orphan :%d",getpid());
+		while(1);
+		}
+	else{
+		printf("\n parent process id : %d",getpid());
+		exit(0);
+
+	}
+return 0;
+}
