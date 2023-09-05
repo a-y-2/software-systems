2ee22bb415ef6901d317cb746064b0ef142a9d22
Ayushi Prasad <60753222+a-y-2@users.noreply.github.com>
2023-09-05 08:24:03 +0530

    Add files via upload

---
+++ 29.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sched.h>
+#include<unistd.h>
+
+int main(){
+	pid_t pid = getpid();
+	int policy;
+	struct sched_param param;
+	param.sched_priority=17;
+	policy = sched_getscheduler(pid);
+	printf("\n current policy : %d",policy);
+	sched_setscheduler(pid,SCHED_FIFO,&param);
+	policy = sched_getscheduler(pid);
+	printf("\n after changing : %d",policy);
+	return 0;
+}
