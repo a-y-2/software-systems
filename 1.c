a9724d406f3e2f382b104acf6ae4fc4e9c1c3a68
Ayushi Prasad <60753222+a-y-2@users.noreply.github.com>
2023-09-08 10:57:54 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,3 +1,13 @@
+/*
+1. Create the following types of a files using (i) shell command (ii) system call
+a. soft link (symlink system call)
+b. hard link (link system call)
+c. FIFO (mkfifo Library Function or mknod system call)
+
+AYUSHI PRASAD
+MT2023145
+*/
+
 #include<stdio.h>
 #include<unistd.h>
 #include<sys/types.h>
