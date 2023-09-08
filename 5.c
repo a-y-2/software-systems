aa897502ff5f52baa11bc4fe972e8719721e5bb3
Ayushi Prasad <60753222+a-y-2@users.noreply.github.com>
2023-09-08 11:01:55 +0530

    Update and rename lab5.c to 5.c

--- 5.c
+++ 5.c
@@ -1,3 +1,10 @@
+/*
+5. Write a program to create five new files with infinite loop. Execute the program in the background and check the file descriptor table at /proc/pid/fd.
+
+AYUSHI PRASAD
+MT2023145
+*/
+
 #include<stdio.h>
 #include<unistd.h>
 #include<sys/types.h>
