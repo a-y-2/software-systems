9f487b2f71b8ad6af5d90feb3041698476ee4a35
Ayushi Prasad <60753222+a-y-2@users.noreply.github.com>
2023-09-08 10:59:10 +0530

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,3 +1,11 @@
+/*
+2. Write a simple program to execute in an infinite loop at the background. 
+Go to /proc directory and identify all the process related information in the corresponding proc directory.
+
+AYUSHI PRASAD
+MT2023145
+*/
+
 #include<stdio.h>
 int main(){
 	while(1){
