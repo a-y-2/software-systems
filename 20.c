e08efc9b0aafea76d17cd2eefffd5257c015858a
Ayushi Prasad <60753222+a-y-2@users.noreply.github.com>
2023-09-10 19:03:25 +0530

    Update 20.c

--- 20.c
+++ 20.c
@@ -1,14 +1,33 @@
+/*
+20. Find out the priority of your running program. Modify the priority with nice command.
 
+AYUSHI PRASAD
+MT2023145
+*/
 #include<sys/types.h>
 #include<fcntl.h>
-#include<sys/stat.h>
+#include<sys/resource.h>
 #include<stdio.h>
-#include<errno.h>
+#include<stdlib.h>
 #include<time.h>
 #include<unistd.h>
 
 int main(int argc,char* argv[]){
-	printf("%d",getpid());
-	while(1);
+        int val;
+        int pid = getpid();
+        val = getpriority(PRIO_PROCESS,0);
+        printf("\npid : %d , nice : %d\n",pid,val);
+        getchar();
+        nice(5);
+        val = getpriority(PRIO_PROCESS,0);
+        printf("\npid : %d , nice : %d\n",pid,val);
+        while(1);
 return 0;
 }
+
+
+
+
+
+
+
