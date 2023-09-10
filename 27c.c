7cd0f5377c68893a2ec7c65a1db833ebc03b1443
Ayushi Prasad <60753222+a-y-2@users.noreply.github.com>
2023-09-10 19:10:32 +0530

    Update 27c.c

--- 27c.c
+++ 27c.c
@@ -1,3 +1,14 @@
+/*
+27. Write a program to execute ls -Rl by the following system calls
+a. execl
+b. execlp
+c. execled. execv
+e. execvp
+
+AYUSHI PRASAD
+MT2023145
+    */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
