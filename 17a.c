2456930eb5c9eb0742b500036235bf0ec43e8994
Ayushi Prasad <60753222+a-y-2@users.noreply.github.com>
2023-09-10 19:00:11 +0530

    Update 17a.c

--- 17a.c
+++ 17a.c
@@ -1,3 +1,13 @@
+/*
+17. Write a program to simulate online ticket reservation. Implement write lock
+Write a program to open a file, store a ticket number and exit. Write a separate program, to
+open the file, implement write lock, read the ticket number, increment the number and print
+the new ticket number then close the file.
+
+AYUSHI PRASAD
+MT2023145
+	*/
+
 #include<sys/types.h>
 #include<fcntl.h>
 #include<sys/stat.h>
