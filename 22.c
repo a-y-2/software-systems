dcac1ccdf0f09d75067ac80e6e2e3925636e8144
Ayushi Prasad <60753222+a-y-2@users.noreply.github.com>
2023-09-10 19:06:41 +0530

    Update 22.c

--- 22.c
+++ 22.c
@@ -1,4 +1,4 @@
-/*Write a program, open a file, call fork, and then write to the file by both the child as well as the
+/*22. Write a program, open a file, call fork, and then write to the file by both the child as well as the
 parent processes. Check output of the file.
 
 AYUSHI PRASAD
