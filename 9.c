a8180e07d335c5594fd598906098ec06d801b311
Ayushi Prasad <60753222+a-y-2@users.noreply.github.com>
2023-09-10 18:47:28 +0530

    Update 9.c

--- 9.c
+++ 9.c
@@ -1,3 +1,20 @@
+/*
+9. Write a program to print the following information about a given file.
+a. inode
+b. number of hard links
+c. uid
+d. gid
+e. size
+f. block size
+g. number of blocks
+h. time of last access
+i. time of last modification
+j. time of last change
+
+AYUSHI PRASAD
+MT2023145
+*/
+
 #include<sys/types.h>
 #include<sys/stat.h>
 #include<stdio.h>
